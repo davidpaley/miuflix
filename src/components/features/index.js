373cc66211173e169628d42720cc5e4651085da4
davidpaley <david.paleyy@gmail.com>
2020-05-17 22:52:59 -0300

    improve content

--- src/components/features/index.js
+++ src/components/features/index.js
@@ -23,35 +23,35 @@ const Features = () => (
               <div className="featureContainer container1">
                 <div className="featureDescription fd1">
                   <h3 className="textAlign">Ver recitales desde tu casa</h3>
-                  <span className="textAlign">Permite la transmicion <strong>en vivo</strong> de conciertos</span>
+                  <span className="textAlign">Permite la transmición <strong>en vivo</strong> de conciertos</span>
                 </div>
                 <div className="imgClass img1" />
               </div>
               <div className="featureContainer container2">
                 <div className="featureDescription fd2">
                   <h3 className="textAlign">Lo mejor para los artistas</h3>
-                  <span className="textAlign">El musico puede dar un concierto desde cualquier sitio</span>
+                  <span className="textAlign">El músico puede dar un concierto desde cualquier sitio</span>
                 </div>
                 <div className="imgClass img2" />
               </div>
               <div className="featureContainer container1">
                 <div className="featureDescription fd1">
                   <h3 className="textAlign">Nuevas experiencias</h3>
-                  <span className="textAlign">Los fanaticos tienen la posibilidad de disfrutar a su artista favorito incluso en el medio de la pandemia</span>
+                  <span className="textAlign">Los fanáticos tienen la posibilidad de disfrutar a su artista favorito incluso en el medio de la pandemia</span>
                 </div>
                 <div className="imgClass img3" />
               </div>
               <div className="featureContainer container2">
                 <div className="featureDescription fd2">
-                  <h3 className="textAlign">Soluciones Economicas</h3>
-                  <span className="textAlign">Los musicos siguen teniendo la posibilidad de seguir recibiendo ingresos, teniendo distintas modalidades como venta de merchandising, interaccion a traves de la aplicacion</span>
+                  <h3 className="textAlign">Beneficios Económicos</h3>
+                  <span className="textAlign">Venta de entradas, merchandising, interacción a través de la aplicación.</span>
                 </div>
                 <div className="imgClass img4" />
               </div>
               <div className="featureContainer container1">
                 <div className="featureDescription fd1">
-                  <h3 className="textAlign">Hablando con el publico</h3>
-                  <span className="textAlign">Distintos tipos de entradas con distintos niveles de interaccion con el artista</span>
+                  <h3 className="textAlign">Hablando con el público</h3>
+                  <span className="textAlign">Distintos tipos de entradas con distintos niveles de interacción con el artista</span>
                 </div>
                 <div className="imgClass img5" />
               </div>
